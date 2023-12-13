863a7321b5e0aae92a39a1478aff68f95430eb88
f4hlevi <149702216+f4hlevi@users.noreply.github.com>
2023-12-13 17:22:24 +0700

    Add files via upload

---
+++ Jobshet-3-Variabel,Konstanta dan Tipe Data/Lat konstanta const.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+	const double PI = 3.14;
+	const char JENIS_KELAMIN = 'P';
+	const int VERSI = 11;
+	
+	printf("Isi konstanta PI adalah %f\n", PI );
+	printf("Isi konstanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);	
+	printf("Isi konstanta VERSI adalah %i\n", VERSI);
+	
+}
+
+
