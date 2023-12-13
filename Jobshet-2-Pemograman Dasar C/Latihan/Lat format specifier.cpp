b7966acf8dfb76326fbce8c8c524f302c9387b56
Fajri Alhidra <149702216+f4hlevi@users.noreply.github.com>
2023-12-13 23:36:52 +0700

    Delete Jobshet-2-Pemograman Dasar C directory

--- Jobshet-2-Pemograman Dasar C/Latihan/Lat format specifier.cpp
+++
@@ -1,9 +0,0 @@
-//created by Fajri Alhidra Fahlevi, nim: 23343062
-#include <stdio.h>
-
-int main(){
-	printf("Hello, ini adalah teks output\n");
-	printf("Nama saya %s\n", "Dian");
-	printf("Usia saya %d\n", 20);
-	return 0;
-}
--- Jobshet-2-Pemograman Dasar C/Latihan/Lat puts.cpp
+++
@@ -1,9 +0,0 @@
-//created by Fajri Alhidra Fahlevi, nim: 23343062
-#include <stdio.h>
-
-int main(){
-	puts("Hello World");
-	puts("Nama Saya Dian");
-	
-	return 0;
-}
